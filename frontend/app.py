3821fcf9aff76f47eac59bd02a02026eba540d25
Ali H <ali.roberto.hdez@gmail.com>
2026-03-31 16:36:05 -0600

    web rendere

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import os
 import flet as ft
 import httpx
 
-API_URL = "https://my-app-t2fs.onrender.com/books"
+API_URL = "https://my-api.onrender.com/books"
 
 
 def main(page: ft.Page):
@@ -21,15 +21,10 @@ def main(page: ft.Page):
 
         page.update()
 
-    page.title = "Flet + FastAPI App"
+    page.title = "Flet + FastAPI"
 
     page.add(ft.ElevatedButton("Cargar libros", on_click=cargar_libros), lista)
 
 
 if __name__ == "__main__":
-    ft.run(
-        main,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        web_renderer="html",
-    )
+    ft.run(main, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
